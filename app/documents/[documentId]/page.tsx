34c4fba6aadca87dd00e9002c2d117d41f407d14
Aaron Rodriguez <69766780+AaronERodriguez@users.noreply.github.com>
2025-07-12 14:31:20 -0600

    `feat: Added support for tables, images, and task lists to the editor`
    `Added @tiptap/extension-image, @tiptap/extension-list, @tiptap/extension-table, and tiptap-extension-resize and also added a undo button

--- app/documents/[documentId]/page.tsx
+++ app/documents/[documentId]/page.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Editor } from './editor';
+import Toolbar from './toolbar';
 
-interface DocumentIdPageProps {
-    params: Promise<{documentId: string}>;
-}
 
-const DocumentIdPage = async ({params}: DocumentIdPageProps) => {
-    const {documentId} = await params
+const DocumentIdPage = async () => {
 
   return (
     <div className='min-h-screen bg-[#FAFBFD]'>
-        <Editor />
+      <Toolbar />
+      <Editor />
     </div>
     
   )
