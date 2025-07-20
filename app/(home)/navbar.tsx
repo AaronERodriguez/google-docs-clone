e7d302c1cf8d03f2f6a79ba1cfed2c526407b0a2
Aaron Rodriguez <69766780+AaronERodriguez@users.noreply.github.com>
2025-07-20 15:55:26 -0600

    added functional clerk

--- app/(home)/navbar.tsx
+++ app/(home)/navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import SearchInput from './search-input'
+import {UserButton} from '@clerk/nextjs';
 
 function Navbar() {
   return (
@@ -13,7 +14,7 @@ function Navbar() {
             <h3 className='text-xl'>Docs</h3>
         </div>
         <SearchInput />
-        <div />
+        <UserButton />
     </nav>
   )
 }
---
+++ components/fullscreen-loader.tsx
@@ -0,0 +1,17 @@
+import { LoaderIcon } from 'lucide-react';
+import React from 'react'
+
+type Props = {
+    label?: string;
+}
+
+const FullScreenLoader = ({label}: Props) => {
+  return (
+    <div className='min-h-screen flex flex-col items-center justify-center gap-2'>
+        <LoaderIcon className='size-6 text-muted-foreground animate-spin' />
+        {label && <p className='text-sm text-muted-foreground'>{label}</p>}
+    </div>
+  )
+}
+
+export default FullScreenLoader
