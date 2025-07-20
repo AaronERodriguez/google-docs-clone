55aca61a420ad0246f0d853b4559079837248e3a
Aaron Rodriguez <69766780+AaronERodriguez@users.noreply.github.com>
2025-07-19 20:47:14 -0600

    added templates

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Navbar from "./navbar";
+import TemplatesGallery from "./templates-gallery";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
         <Navbar />
       </div>
       <div className="mt-16">
-      Click <Link href={'/documents/123'}>here</Link> to go to document id
+        <TemplatesGallery />
       </div>
     </div>
   );
